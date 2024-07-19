6fd65076d710d172b50b88dbddcd87dc0a7f80b9
balar2207 <135403084+balar2207@users.noreply.github.com>
2024-07-19 21:39:55 +0530

    Update unregister-rhn.sh

--- unregister-rhn.sh
+++ unregister-rhn.sh
@@ -1,3 +1,12 @@
 #/bin/bash
-subscription-manager remove --all
-subscription-manager unregister
+echo "Do you want to remove the rhn-subsription for this machine..?"
+read yes
+if [ "yes" == "$yes" ];
+then
+        subscription-manager remove --all
+        subscription-manager unregister
+elif [ "no" ==  "$no" ];
+then
+        exit
+fi
+~
