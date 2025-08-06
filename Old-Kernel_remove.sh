7a3e76f656f79d90a59293c15a95d83ef714c285
balar2207 <135403084+balar2207@users.noreply.github.com>
2025-08-06 17:04:59 +0530

    Old-Kernel_remove.sh

--- Old-Kernel_remove.sh
+++ Old-Kernel_remove.sh
@@ -1,14 +1,19 @@
-dnf -y remove --oldinstallonly --setopt installonly_limit=2 kernel
+#!/bin/bash
+
+echo "$(dnf -y remove --oldinstallonly --setopt installonly_limit=2 kernel)"
 sleep 10
 echo "The old kernel has been removed and the system is rebooting now"
 reboot
-#!/bin/bash
 
-echo "$(curl -O https://github.com/balar2207/Linux-VMs-scripts/blob/main/Old-Kernel_remove.sh)"
-sleep 5
-sed -i 's/\r$//' Old-Kernel_remove.sh
-chmod +x Old-Kernel_remove.sh
-echo "$(./Old-Kernel_remove.sh)"
-echo "Removing the old Kenrnel"
-sleep 20
-rm -rf ld-Kernel_remove.sh
+
+
+#sleep 5
+#sed -i 's/\r$//' Old-Kernel_remove.sh
+#chmod +x Old-Kernel_remove.sh
+#echo "$(./Old-Kernel_remove.sh)"
+#echo "Removing the old Kenrnel"
+#sleep 20
+#rm -rf ld-Kernel_remove.sh
+
+
+#echo "$(curl -O https://github.com/balar2207/Linux-VMs-scripts/blob/main/Old-Kernel_remove.sh)"
